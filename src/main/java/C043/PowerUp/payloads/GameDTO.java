865b1d3cabbc7c8a7ab5a1ee3cb4f3d200710fed
Mario Fragnito <mariofragnitoph@gmail.com>
2025-06-29 04:39:20 +0200

    Last minute edits

--- src/main/java/C043/PowerUp/payloads/GameDTO.java
+++ src/main/java/C043/PowerUp/payloads/GameDTO.java
@@ -4,10 +4,6 @@ import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Range;
 
 public record GameDTO(
-        @NotNull(message = "Game id is required")
-        int gameId,
-        @Range(min = 0, max = 5, message = "Rating must be in a range from 1 " +
-                "to 5")
-        int userRating
-) {
-}
+    @NotNull(message = "Game id is required") int gameId,
+    @Range(min = 0, max = 5, message = "Rating must be in a range from 1 " + "to 5")
+        int userRating) {}
